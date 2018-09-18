a51fa9f93804a5369766e8a1fc0b85e1bc64ee58
Simon Knott <simoknott@gmail.com>
2018-09-18 17:23:55 +0200

    up_dev now takes args for docker compose

--- scripts/up_dev.sh
+++ scripts/up_dev.sh
@@ -4,6 +4,6 @@ touch docker-compose.out.yml
 echo "" > docker-compose.out.yml
 docker-app render > docker-compose.out.yml
 
-docker-compose -f docker-compose.out.yml -f docker-compose.dev.yml up
+docker-compose -f docker-compose.out.yml -f docker-compose.dev.yml up "$@"
 
 rm docker-compose.out.yml
